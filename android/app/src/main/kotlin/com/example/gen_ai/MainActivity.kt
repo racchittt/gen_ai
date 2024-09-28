c86ff68915ef65c52819158f9a0ae58190af1971
racchittt <112688112+racchittt@users.noreply.github.com>
2024-09-29 02:22:06 +0530

    feat:initial commit

---
+++ android/app/src/main/kotlin/com/example/gen_ai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gen_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:gen_ai/type.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+    body: Type(),
+  )));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('data');
+  }
+}
