146652f45e623fa337050396a611b31ad0f78d55
sahil <shsasane22@gmail.com>
2024-10-17 20:02:58 +0530

    fix: fine tuned gemini

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -1,23 +1,47 @@
-
-const { GoogleGenerativeAI } = require("@google/generative-ai");
 const env = process.env;
-
-const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
+const axios = require('axios');
 
 async function geminiResponse(body) {
-    try {
+    let data = JSON.stringify({
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": body
+                    }
+                ]
+            }
+        ],
+        "generationConfig": {
+            "temperature": 0.8,
+            "topK": 64,
+            "topP": 0.95,
+            "maxOutputTokens": 8192,
+            "responseMimeType": "text/plain"
+        }
+    });
 
-        console.log(env.GEMINI_API_KEY)
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        console.log(body);
-        let prompt = env.GEN_AI_PROMPT
-        console.log(prompt)
-        const result = await model.generateContent(prompt + body);
-        const response = result.response;
-        const text = response.text();
-        console.log(text);
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: env.GEMINI_URL,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    try {
+        const response = await axios.request(config);
+        let text = response.data.candidates[0].content.parts[0].text
+        if (text.startsWith("bot:")) {
+            text = text.substring(4);
+        }
+        console.log(JSON.stringify(response.data));
+
         return text;
     } catch (error) {
+        console.log(error);
         throw error;
     }
 }
@@ -25,3 +49,5 @@ async function geminiResponse(body) {
 module.exports = {
     geminiResponse: geminiResponse
 }
+
+
